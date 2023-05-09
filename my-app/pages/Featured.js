d24a2cf6a024f148f0477e44ab8a8b702b8850b2
NanaNiki <nicol.wesolowska@gmail.com>
2023-05-09 22:21:09 +0200

    "/catalouge/favourites done"

--- my-app/pages/Featured.js
+++ my-app/pages/Featured.js
@@ -4,6 +4,34 @@ import plantsData from "./product/plants.json";
 import Link from "next/link.js";
 
 export default function Featured() {
+  const favTegCheck = () => {
+    const favouritePlants = plantsData.filter(
+      (plant) => plant.id <= 3 );
+    return favouritePlants.map((plant, index) => {
+      return (
+        <span
+                key={index}
+                className="flex flex-col lg:flex-shrink-0 lg:me-6 sm:me-8"
+              >
+              <Link href={`/product/${plant.id}`} passHref>
+                <Image
+                  src={plant.image}
+                  width={270}
+                  height={500}
+                  alt={`Plant product ${plant.name}`}
+                  className="inline-block hover:scale-95 ease-in-out duration-300"
+                />
+                <div
+                  className={` flex flex-row justify-between p-2 text-end lg:text-xl md:text-base sm:text-sm text-xs ${castoro.className}`}
+                >
+                  <h5>{plant.name}</h5> <h5>{plant.price}</h5>
+                </div>
+                </Link>
+              </span>
+      );
+    });
+  };
+
   return (
     <section className="h-full ms-6" id="featured">
       <div className="flex flex-col justify-center m-auto lg:p-32 md:p-20 sm:p-16 p-10 lg:text-4xl md:text-3xl sm:text-2xl text-xl text-center">
@@ -26,39 +54,17 @@ export default function Featured() {
             <p className="mb-4">harmful chemicals.</p>
             But please don't eat them
           </span>
-          <button className="hidden sm:block lg:my-10 md:my-5 sm:my-3 cursor-pointer bg-stone-200 w-40 h-10 text-sm font-bold underline underline-offset-4 hover:bg-stone-500 hover:text-stone-50 hover:animate-bounce transition-all duration-200">
+          <Link className="hidden sm:block lg:my-10 md:my-5 sm:my-3 pt-2 cursor-pointer bg-stone-200 w-40 h-10 text-sm text-center font-bold underline underline-offset-4 hover:bg-stone-500 hover:text-stone-50 hover:animate-bounce transition-all duration-200" href={'/catalogue/favourites'} passHref>
             Shop all Favourites
-          </button>
+          </Link>
         </div>
         <div className="flex overflow-x-scroll scroll-smooth justify-center md:justify-start">
-          {plantsData.map((plant, index) => {
-            return (
-              <span
-                key={index}
-                className="flex flex-col lg:flex-shrink-0 lg:me-6 sm:me-8"
-              >
-              <Link href={`/product/${plant.id}`} passHref>
-                <Image
-                  src={plant.image}
-                  width={270}
-                  height={500}
-                  alt={`Plant product ${plant.name}`}
-                  className="inline-block hover:scale-95 ease-in-out duration-300"
-                />
-                <div
-                  className={` flex flex-row justify-between p-2 text-end lg:text-xl md:text-base sm:text-sm text-xs ${castoro.className}`}
-                >
-                  <h5>{plant.name}</h5> <h5>{plant.price}</h5>
-                </div>
-                </Link>
-              </span>
-            );
-          })}
+          {favTegCheck(plantsData)}
         </div>
         <div className="flex flex-row w-screen justify-end lg:hidden md:hidden sm:hidden">
-          <button className="m-4 cursor-pointer bg-stone-200 w-40 h-10 text-xs font-bold underline underline-offset-4 hover:bg-stone-500 hover:text-stone-50 hover:animate-bounce transition-all duration-200">
+        <Link className="m-4 cursor-pointer bg-stone-200 w-40 h-10 text-xs font-bold underline underline-offset-4 hover:bg-stone-500 hover:text-stone-50 hover:animate-bounce transition-all duration-200" href={'/catalogue/favourites'} passHref>
             Shop all Favourites
-          </button>
+          </Link>
         </div>
       </div>
     </section>
---
+++ my-app/pages/catalogue/favourites.js
@@ -0,0 +1,52 @@
+import { castoro } from "../index.js";
+import { inter } from "../index.js";
+import Link from "next/link";
+import Image from "next/image.js";
+import NavBar from "../NavBar.js";
+import Footer from "../Footer.js";
+import plantsData from "../product/plants.json";
+import { BsSuitHeart } from "react-icons/bs";
+
+export default function Favourites() {
+  const favTegCheck = () => {
+    const favouritePlants = plantsData.filter(
+      (plant) => plant.favourites === true
+    );
+    return favouritePlants.map((plant, index) => {
+      return (
+        <div key={index} className="w-fit lg:pe-6 shrink-0">
+          <Link href={`/product/${plant.id}`} passHref target="_parent">
+            <Image
+              src={plant.image}
+              width={270}
+              height={100}
+              alt={`Plant product ${plant.name}`}
+              className="lg:mb-2 inline-block hover:scale-95 ease-in-out duration-300"
+            />
+            <div
+              className={` flex flex-row lg:justify-between p-1 text-end md:text-base sm:text-sm text-xs ${inter.className}`}
+            >
+              <h5 className="p1-2 lg:ps-0">{plant.name}</h5> <h5 className="font-bold ps-2 lg:ps-0">{plant.price}</h5>
+            </div>
+          </Link>
+        </div>
+      );
+    });
+  };
+
+  return (
+    <div id="favourites">
+      <NavBar />
+      <div
+        className={` mt-14 flex flex-row w-fit mx-auto lg:text-5xl md:text-4xl sm:text-3xl text-2xl text-center lg:my-20 my-10 font-bold ${castoro.className}`}
+        id="catalogue"
+      >
+        Our Fa<BsSuitHeart  className="lg:w-7 lg:h-7 lg:mt-2 w-5 h-5 text-center mt-1.5"/>ourites
+      </div>
+      <div className="flex flex-row overflow-x-scroll scroll-smooth justify-start mx-auto pb-10">
+        {favTegCheck(plantsData)}
+      </div>
+      <Footer />
+    </div>
+  );
+}
